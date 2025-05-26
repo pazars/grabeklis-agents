47fa9673fdf3dc7e2fd4820d8149156961b25637
davispazars <davis.pazars@gmail.com>
2025-05-26 19:07:55 +0300

    Basic summary agent

---
+++ summary_agent/agent.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from google.genai import types
+from google.adk.agents import Agent
+from .prompts import SUMMARY_SYSTEM_PROMPT
+
+
+class Article(BaseModel):
+    title: str
+    url: str
+    summary: str
+
+class Summary(BaseModel):
+    category: str
+    articles: list[Article]
+
+class ResponseSchema(BaseModel):
+    category_summary: list[Summary]
+
+
+config = types.GenerateContentConfig(
+    max_output_tokens=4096,
+)
+root_agent = Agent(
+    name="summary_agent",
+    model="gemini-2.0-flash",
+    description=("News article summary assistant"),
+    instruction=(
+        SUMMARY_SYSTEM_PROMPT),
+    output_schema=ResponseSchema,
+)
---
+++ summary_agent/prompts.py
@@ -0,0 +1,9 @@
+SUMMARY_SYSTEM_PROMPT="Tu esi pieredzējis ziņu redaktors. " \
+"Tavs uzdevums ir radīt precīzus, īsus un informatīvus ziņu rakstu kopsavilkumus. " \
+"Kopsavilkumam jāatspoguļo raksta būtība, jāizceļ galvenie fakti un notikumi, " \
+"izvairoties no subjektīviem vērtējumiem vai spekulācijām. Raksti ne vairāk kā 3–5 teikumus. " \
+"Izmanto skaidru un neitrālu valodu. Sagrupē rakstus pa tēmām un rakstus par vienu un to pašu tēmu apvieno. " \
+"Tev ir dotas oriģinālās ziņu rakstu kategorijas, kas var palīdzēt grupēšanai, bet tu drīksti tās mainīt un uzlabot."
+"Pirmos izcel rakstus saistītus ar svarīgiem notikumiem Latvijā, tad Eiropas un tad pasaules notikumus, " \
+"pēc tam specifiskākas tēmas sportu, kultūru, tehnoloģijas. " \
+"Pārējo beigās. Centies neizlaist rakstus, lai sniegtu detalizētu pārskatu."
